f49c88ebd6ec0c90100501ea59e489512770a1b5
Sierra Mills <sierratmills@gmail.com>
2018-06-29 14:19:03 +0200

    add content to slides on the main page

--- src/pages/new-account/new-account.ts
+++ src/pages/new-account/new-account.ts
@@ -17,85 +17,107 @@ import { MainPage } from '../main/main';
 })
 export class NewAccountPage {
 
-  protected firstName='';
-  protected lastName='';
-  protected username='';
-  public passwordCheck='';
-  protected password='';
-  protected email='';
-  
+  private firstName = '';
+  private lastName = '';
+  private username = '';
+  public passwordCheck = '';
+  private password = '';
+  private email = '';
+  private newEmail = false;
+  private goodPassword = false;
+  private newUsername = false;
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
-      
-  }
 
-  showToast() {
-    let toast = this.toastCtrl.create({
-      message: 'How to use this app: ...',
-      showCloseButton: true,
-      position: "middle"
-    });
-    toast.present().then(() => {
-      toast.onDidDismiss(() => {
-        this.navigateToMain();
-      });
-    });
-  }
 
-  checkValidPassword(){
-    var uppercase = false;
-    var lowercase = false;
-    var specialcharacter = false;
-    var number = false;
-    
-    var loop = 0;
-    for (loop = 0; loop < this.password.length; loop++){
-      if (this.password.charCodeAt(loop) >= 33 && this.password.charCodeAt(loop) <= 47
-        ||this.password.charCodeAt(loop) >= 58 && this.password.charCodeAt(loop) <= 64 
-        ||this.password.charCodeAt(loop) >= 91 && this.password.charCodeAt(loop) <= 96
-        ||this.password.charCodeAt(loop) >= 123 && this.password.charCodeAt(loop) <= 126){
-          specialcharacter = true;
-      } else if (this.password.charCodeAt(loop) >= 48 && this.password.charCodeAt(loop) <= 57){
-        number = true;
-      } else if (this.password.charCodeAt(loop) >= 65 && this.password.charCodeAt(loop) <= 90){
-        uppercase = true;
-      } else if (this.password.charCodeAt(loop) >= 97 && this.password.charCodeAt(loop) <= 122){
-        lowercase = true;
-      }
-    }
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
 
-    if (!(uppercase && lowercase && number && specialcharacter && this.password.length < 6)){
-      //prompt user to enter a valid password;
-    }
+  }
 
-    return true;
-  }
+  showToast() {
+    let toast = this.toastCtrl.create({
+      message: 'How to use this app: ...',
+      showCloseButton: true,
+      position: "middle"
+    });
+    toast.present().then(() => {
+      toast.onDidDismiss(() => {
+        this.navigateToMain();
+      });
+    });
+  }
 
-  checkEmailIsNew(){
-    //see if email has been used before
-    return true;
-  }
+  checkValidPassword() {
+    var uppercase = false;
+    var lowercase = false;
+    var specialcharacter = false;
+    var number = false;
+    var matching = false;
 
-  checkUsername() {
-    //check to see if username exists, if it does, prompt user to choose a different username
-  }
+    var loop = 0;
+    for (loop = 0; loop < this.password.length; loop++) {
+      if ((this.password.charCodeAt(loop) >= 33 && this.password.charCodeAt(loop) <= 47)
+        || (this.password.charCodeAt(loop) >= 58 && this.password.charCodeAt(loop) <= 64)
+        || (this.password.charCodeAt(loop) >= 91 && this.password.charCodeAt(loop) <= 96)
+        || (this.password.charCodeAt(loop) >= 123 && this.password.charCodeAt(loop) <= 126)) {
+        specialcharacter = true;
+      } else if (this.password.charCodeAt(loop) >= 48 && this.password.charCodeAt(loop) <= 57) {
+        number = true;
+      } else if (this.password.charCodeAt(loop) >= 65 && this.password.charCodeAt(loop) <= 90) {
+        uppercase = true;
+      } else if (this.password.charCodeAt(loop) >= 97 && this.password.charCodeAt(loop) <= 122) {
+        lowercase = true;
+      } else if (this.password === this.passwordCheck) {
+        matching = true;
+      }
+    }
 
-  addInfoToDatabase(){
-    //add user info to a database
-  }
+    if (!(uppercase && lowercase && number && matching && specialcharacter && this.password.length < 6)) {
+      this.goodPassword = false;
+    }
 
-navigateToProfile() {
-  console.log("Navigating..");
-  this.navCtrl.push(ProfilePage);
-}
+    this.goodPassword = true;;
+  }
 
-navigateToMain() {
-  console.log("Navigating..");
-  this.navCtrl.push(MainPage);
-}
+  checkEmailIsNew() {
+    //see if email has been used before
+    this.newEmail = true;
+  }
 
-ionViewDidLoad() {
-  console.log('ionViewDidLoad NewAccountPage');
-}
+  checkUsername() {
+    //check to see if username exists, if it does, prompt user to choose a different username
+    this.newUsername = true;
+  }
+
+  checkAll() {
+    this.checkEmailIsNew();
+    this.checkUsername();
+    this.checkValidPassword();
+
+    if (this.newUsername && this.newEmail && this.goodPassword) {
+      this.addInfoToDatabase();
+      this.showToast();
+    } else if (!this.newUsername) {
+      //toast 
+      //checkAll();
+    }
+  }
+
+  addInfoToDatabase() {
+    //add user info to a database
+  }
+
+  navigateToProfile() {
+    console.log("Navigating..");
+    this.navCtrl.push(ProfilePage);
+  }
+
+  navigateToMain() {
+    console.log("Navigating..");
+    this.navCtrl.push(MainPage);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NewAccountPage');
+  }
 
 }
